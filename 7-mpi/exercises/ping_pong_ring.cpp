b6ac00e4e4130cfcf8f6e834e56f485efbdababf
PaulAroo <timmypaul2006@gmail.com>
2024-11-30 13:00:41 +0100

    wip: mpi exercises

--- 7-mpi/exercises/ping_pong_ring.cpp
+++ 7-mpi/exercises/ping_pong_ring.cpp
@@ -24,6 +24,25 @@ int main(int argc, char **argv)
   MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
 
   /** TODO: ping pong ring. */
+  std::string ping("ping");
+  std::string rdata(4, ' ');
+  int next_rank = (rank + 1) % num_procs;
+  int prev_rank = ((rank + num_procs) - 1) % num_procs;
+
+  MPI_Request rq_send;
+  MPI_Request rq_receive;
+  int flag;
+
+  printf("%d: send ping to %d...\n", rank, next_rank);
+  MPI_Isend(ping.data(), ping.size(), MPI_CHAR, next_rank, 0, MPI_COMM_WORLD, &rq_send);
+
+
+  printf("%d: receive ping from %d...\n", rank, prev_rank);
+  MPI_Recv(rdata.data(), rdata.size(), MPI_CHAR, prev_rank, 0, MPI_COMM_WORLD);
+
+  MPI_Wait(&rq_send, MPI_STATUS_IGNORE);
+
+  // send pong
 
   /* Tear down the communication infrastructure */
   MPI_Finalize();
